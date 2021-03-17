fd99e9960e70abec7354a39e67192b80ab2dbe59
Tejas H <tejas007tez@gmail.com>
2021-03-18 01:24:01 +0530

    Add socket communication layer

---
+++ src/Data/socket.js
@@ -0,0 +1,36 @@
+let client;
+
+const socket_URL = process.env.REACT_APP_SOCKET_URL || 'wss://city-ws.herokuapp.com/';
+
+export const initiateSocketConnection = () => {
+  client = new WebSocket(socket_URL);
+};
+
+export const subscribeSocket = callback => {
+  if (!client) {
+    initiateSocketConnection();
+  }
+
+  client.onerror = event => {
+    console.log('Connection Error');
+  };
+
+  client.onopen = event => {
+    console.log('Connected');
+  };
+
+  client.onclose = event => {
+    console.log('Closed');
+  };
+
+  client.onmessage = event => {
+    return callback(JSON.parse(event.data));
+  };
+};
+
+export const unsubscribeSocket = () => {
+  if (!client) {
+    return;
+  }
+  client.close();
+};

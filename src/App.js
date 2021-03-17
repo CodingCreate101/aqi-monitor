2ed82cbf76dcd1128cc82e0ccb316233a6b2e446
Tejas H <CodingCreate101@users.noreply.github.com>
2021-03-18 01:30:49 +0530

    Add socket communication layer (#3)
    * Add socket communication layer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import './App.css';
+import HomePage from './Pages/HomePage/HomePage';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <HomePage />
     </div>
   );
 }
